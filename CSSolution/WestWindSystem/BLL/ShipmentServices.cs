eb1315d3d19ba649b1fcd0ec8894f011d4dcd709
Don Welch <dwelch@nait.ca>
2025-03-21 18:33:04 -0600

    alter shipment service query used in scrolling to paging versions of query, consume paging versions on webpage in GetQuery event and OnPageChanged event

--- CSSolution/WestWindSystem/BLL/ShipmentServices.cs
+++ CSSolution/WestWindSystem/BLL/ShipmentServices.cs
@@ -52,6 +52,9 @@ namespace WestWindSystem.BLL
         //note the predicate uses the virtual navigational property of the Shipment entity
         //This will include the associated record from the Shippers table (parent) for
         //      the shipment record (child)
+
+        //using this form of the query for scrolling
+        //returns the entire query collection
         public List<Shipment> Shipment_GetByYearAndMonth(int year, int month)
         {
             //it is possible to place validation of incoming parameters within your services
@@ -76,6 +79,77 @@ namespace WestWindSystem.BLL
                                                  .OrderBy(s => s.ShippedDate);
             return info.ToList();
         }
+
+        //Pagination
+
+        //return the total number of records that would be returned for the query
+        //this query will NOT return any actual query result records
+
+        public int Shipment_GetByYearAndMonthCount(int year, int month)
+        {
+            if (year < 1950 || year > DateTime.Today.Year)
+            {
+                throw new ArgumentException($"Invalid year {year}. Year must be between 1950 and today");
+            }
+            if (month < 1 || month > 12)
+            {
+                throw new ArgumentException($"Invalid month {month}. Month must be between 1 and 12");
+            }
+            //execute the query without any additional methods use to join other tables or organize the 
+            //   queried dataset
+            IEnumerable<Shipment> info = _context.Shipments
+                                                .Where(s => s.ShippedDate.Year == year
+                                                        && s.ShippedDate.Month == month);
+            return info.Count(); //just get the count of the dataset records
+
+            // alternative Linq statements
+            //return _context.Shipments
+            //                .Where(s => s.ShippedDate.Year == year
+            //                        && s.ShippedDate.Month == month)
+            //                .Count();
+
+            //return _context.Shipments
+            //               .Count(s => s.ShippedDate.Year == year
+            //                       && s.ShippedDate.Month == month);
+        }
+
+        //this method will return the data set records that are NEEDED for the current page
+        //it does NOT return the entire data set collection
+        //the method needs to determine the record subset to return
+
+        public List<Shipment> Shipment_GetByYearAndMonthPaging(int year, 
+                                                                int month,
+                                                                int currentpagenumber,
+                                                                int itemsperpage)
+        {
+            //the currentpagenumber and itemsperpage are used in the determination of which
+            //  dataset record subset is to be returned from the entire dataset query collection
+
+            if (year < 1950 || year > DateTime.Today.Year)
+            {
+                throw new ArgumentException($"Invalid year {year}. Year must be between 1950 and today");
+            }
+            if (month < 1 || month > 12)
+            {
+                throw new ArgumentException($"Invalid month {month}. Month must be between 1 and 12");
+            }
+            IEnumerable<Shipment> info = _context.Shipments
+                                                .Include(s => s.ShipViaNavigation)
+                                                .Where(s => s.ShippedDate.Year == year
+                                                        && s.ShippedDate.Month == month)
+                                                .OrderBy(s => s.ShippedDate);
+
+            //pagination calculation logic
+            //calculate the number of records to skip
+            //subtract 1 from the natural page number to get the page index number
+            int recordsSkipped = itemsperpage * (currentpagenumber - 1);
+
+            //return JUST the records for the current page
+            //Skip: skip the first x items representing previous pages
+            //Take: take up to the necessary number of items on a page
+            return info.Skip(recordsSkipped).Take(itemsperpage).ToList();
+        }
+
         #endregion
     }
 }
