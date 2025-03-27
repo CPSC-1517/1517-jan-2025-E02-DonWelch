640cd8d017878df23a0ca9b490e4f1ad22c49457
Don Welch <dwelch@nait.ca>
2025-03-26 18:52:04 -0600

    complete the query page of a search/crud two page demo, import starting kit files, add a link to query table line to pass a value to another page.

---
+++ CSSolution/WestWindSystem/BLL/ProductServices.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore; //needed for the .Include
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class ProductServices
+    {
+        #region setup the context connection variable and class constructor
+        private readonly WestWindContext _context;
+
+        //constructor to be used in the creation of the instance of this class
+        //the registered reference for the context connection (database connection)
+        //  will be passed from the IServiceCollection registered services
+        internal ProductServices(WestWindContext registeredcontext)
+        {
+            _context = registeredcontext;
+        }
+        #endregion
+
+        //Queries
+
+        public List<Product> Product_GetByCategoryID(int categoryid)
+        {
+            //IEnumerable<Product> info = _context.Products
+            //                                    .Where(x => x.CategoryID == categoryid)
+            //                                    .OrderBy(x => x.ProductName);
+            //return info.ToList();
+
+            //alternate
+            List<Product> info = _context.Products
+                                               .Where(x => x.CategoryID == categoryid)
+                                               .OrderBy(x => x.ProductName)
+                                               .ToList();
+            return info;
+
+        }
+
+        //if you wish to use the Include technique to obtain the Supplier company name
+        //public List<Product> Product_GetByCategoryID(int categoryid)
+        //{
+
+        //    List<Product> info = _context.Products
+        //                                        .Include(x => x.Supplier)
+        //                                       .Where(x => x.CategoryID == categoryid)
+        //                                       .OrderBy(x => x.ProductName)
+        //                                       .ToList();
+        //    return info;
+        //}
+
+       
+                            
+    }
+}
--- CSSolution/WestWindSystem/WestWindExtensions.cs
+++ CSSolution/WestWindSystem/WestWindExtensions.cs
@@ -70,6 +70,24 @@ namespace WestWindSystem
                 var context = serviceProvider.GetService<WestWindContext>();
                 return new ShipmentServices(context);
             });
+
+            services.AddTransient<CategoryServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new CategoryServices(context);
+            });
+
+            services.AddTransient<ProductServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new ProductServices(context);
+            });
+
+            services.AddTransient<SupplierServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new SupplierServices(context);
+            });
         }
     }
 }
